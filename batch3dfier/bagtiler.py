cf5415c6531e03964092b5346f405e8599aac06f
Balázs Dukai <balazs.dukai@gmail.com>
2017-06-28 23:32:29 +0200

    update bagtiler to use externally

--- batch3dfier/bagtiler.py
+++ batch3dfier/bagtiler.py
@@ -24,56 +24,98 @@
 #===============================================================================
 
 from psycopg2 import sql
-import psycopg2
 
 
-# Connect to database
-try:
-    conn = psycopg2.connect(
-        database="bag", user="bag", password="bag2017", host="localhost",
-        port="5432")
-    print("Opened database successfully")
-except:
-    print("I'm unable to connect to the database")
-cur = conn.cursor()
+def create_centroid_table(conn):
+    """Creates a table of building footprint centroids in a standard BAG database
 
+    Parameters
+    ----------
+    conn : psycopg2 Connection object
+        
 
-# Create schema to store the tiles
-schema = sql.Identifier("bag_tiles")
-cur.execute(sql.SQL("CREATE SCHEMA IF NOT EXISTS {};").format(schema))
-conn.commit()
+    Returns
+    -------
+    nothing
 
+    """
+    
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE bagactueel.pand_centroid AS
+                SELECT gid, st_centroid(geovlak)::geometry(point, 28992) AS geom
+                FROM bagactueel.pandactueelbestaand;
+            
+            SELECT populate_geometry_columns('bagactueel.pand_centroid'::regclass);
+            
+            CREATE
+                INDEX pand_centroid_geom_idx ON
+                bagactueel.pand_centroid
+                    USING gist(geom);
+            """)
+    conn.commit()
+    
+    with conn.cursor() as cur:
+        cur.execute("VACUUM ANALYZE bagactueel.pand_centroid;")
+    conn.commit()
+    
+    
+    
+def bagtiler(conn):
+    """Creates the BAG tiles (or Views) in the bag_tiles schema.
 
-# Get AHN3 tile IDs
-cur.execute("SELECT unit FROM ahn3.ahn_units;")
-tiles = [i[0] for i in cur.fetchall()]
+    Parameters
+    ----------
+    conn : psycopg2 Connection object
+        
 
+    Returns
+    -------
+    nothing
 
-# Create a BAG tile with equivalent area of an AHN tile
-for tile in tiles:
-    # the 't_' prefix is hard-coded in config.call3dfier()
-    view = sql.Identifier('t_' + tile)
-    cur.execute(
-        sql.SQL("""CREATE OR REPLACE VIEW {schema}.{view} AS
-                    SELECT
-                        b.gid,
-                        b.identificatie,
-                        b.geovlak
-                    FROM
-                        bagactueel.pand b
-                    INNER JOIN bagactueel.pand_centroid c ON
-                        b.gid = c.gid,
-                        ahn3.ahn_units a
-                    WHERE
-                        a.unit = %s
-                        AND(
-                            st_containsproperly(
-                                a.geom,
-                                c.geom
-                            )
-                            OR st_contains(
-                                a.geom_border,
-                                c.geom
-                            )
-                        );""").format(schema=schema, view=view), [tile])
-conn.commit()
+    """
+    with conn.cursor() as cur:
+        # Create schema to store the tiles
+        schema = sql.Identifier("bag_tiles")
+        cur.execute(sql.SQL("CREATE SCHEMA IF NOT EXISTS {};").format(schema))
+        conn.commit()
+        
+        
+        # Get AHN3 tile IDs
+        cur.execute("SELECT unit FROM ahn3.ahn_units;")
+        tiles = [i[0] for i in cur.fetchall()]
+        
+        
+        # Create a BAG tile with equivalent area of an AHN tile
+        for tile in tiles:
+            # the 't_' prefix is hard-coded in config.call3dfier()
+            view = sql.Identifier('t_' + tile)
+            cur.execute(
+                sql.SQL("""CREATE OR REPLACE VIEW {schema}.{view} AS
+                            SELECT
+                                b.gid,
+                                b.identificatie,
+                                b.geovlak
+                            FROM
+                                bagactueel.pand b
+                            INNER JOIN bagactueel.pand_centroid c ON
+                                b.gid = c.gid,
+                                ahn3.ahn_units a
+                            WHERE
+                                a.unit = %s
+                                AND(
+                                    st_containsproperly(
+                                        a.geom,
+                                        c.geom
+                                    )
+                                    OR st_contains(
+                                        a.geom_border,
+                                        c.geom
+                                    )
+                                );""").format(schema=schema, view=view), [tile])
+        try:
+            conn.commit()
+            print("%s BAG tiles created in schema 'bag_tiles'." % (len(tiles)))
+        except:
+            conn.rollback()
+            print("Couldn't create BAG tiles in schema 'bag_tiles'. Rolling back transaction.")
