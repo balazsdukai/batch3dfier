76f055a9e3d48f40e128b5a9eb4087a8a9e4c11c
Balázs Dukai <balazs.dukai@gmail.com>
2017-07-17 22:41:14 +0200

    bagtiler() more generic, but not completely

--- batch3dfier/bagtiler.py
+++ batch3dfier/bagtiler.py
@@ -131,47 +131,59 @@ def create_centroid_table(db, schema, table_centroid, table_poly, id_col, geom_c
     db.vacuum(schema, table_centroid)
 
 
-def bagtiler(db):
-    """Creates the BAG tiles (or Views) in the bag_tiles schema.
+def bagtiler(db, schema_tiles, tile_index, centroid_table):
+    """Creates the BAG tiles (or Views).
 
     Parameters
     ----------
     db : db Class instance
-        
+    schema_tiles : str 
+        Name of the schema where to create the tiles.
+    tile_index : tuple
+        (schema, table, field) that contains the tile index.
+    centroid_table : tuple
+        (schema, table) that contains the 2D polygon centroids.
 
     Returns
     -------
-    boolean
-        indicating success/failure
+    nothing
 
     """
     # Create schema to store the tiles
-    schema = sql.Identifier("bag_tiles")
-    query = sql.SQL("CREATE SCHEMA IF NOT EXISTS {};").format(schema)
+    schema_tiles_q = sql.Identifier(schema_tiles)
+    query = sql.SQL("CREATE SCHEMA IF NOT EXISTS {};").format(schema_tiles_q)
     db.sendQuery(query)
         
-            # Get AHN3 tile IDs
-    tiles = db.getQuery("SELECT unit FROM ahn3.ahn_units;")
+    # Get AHN3 tile IDs
+    schema_idx_q = sql.Identifier(tile_index[0])
+    table_idx_q = sql.Identifier(tile_index[1])
+    field_idx_q =sql.Identifier(tile_index[2])
+    tiles = db.getQuery(
+        sql.SQL("SELECT {} FROM {}.{};").format(field_idx_q, schema_idx_q,
+                                                table_idx_q)
+                        )
     tiles = [i[0] for i in tiles]
-        
+    
+    schema_ctr_q = sql.Identifier(centroid_table[0])
+    table_ctr_q = sql.Identifier(centroid_table[1])
         
     # Create a BAG tile with equivalent area of an AHN tile
     for tile in tiles:
         # the 't_' prefix is hard-coded in config.call3dfier()
         view = sql.Identifier('t_' + tile)
         tile = sql.Literal(tile)
-        query = sql.SQL("""CREATE OR REPLACE VIEW {schema}.{view} AS
+        query = sql.SQL("""CREATE OR REPLACE VIEW {schema_tiles}.{view} AS
                         SELECT
                             b.gid,
                             b.identificatie,
                             b.geovlak
                         FROM
                             bagactueel.pand b
-                        INNER JOIN bagactueel.pand_centroid c ON
+                        INNER JOIN {schema_ctr}.{table_ctr} c ON
                             b.gid = c.gid,
-                            ahn3.ahn_units a
+                            {schema_idx}.{table_idx} a
                         WHERE
-                            a.unit = {tile}
+                            a.{field_idx} = {tile}
                             AND(
                                 st_containsproperly(
                                     a.geom,
@@ -181,13 +193,19 @@ def bagtiler(db):
                                     a.geom_border,
                                     c.geom
                                 )
-                        );""").format(schema=schema, view=view, tile=tile)
+                        );""").format(schema_tiles=schema_tiles_q, view=view,
+                                      tile=tile, schema_idx=schema_idx_q,
+                                      table_idx=table_idx_q,
+                                      field_idx=field_idx_q,
+                                      schema_ctr=schema_ctr_q,
+                                      table_ctr=table_ctr_q)
         db.sendQuery(query)
     
+    # db.conn.commit() is totally redundant
     try:
         db.conn.commit()
-        print("%s BAG tiles created in schema 'bag_tiles'." % (len(tiles)))
+        print("%s BAG tiles created in schema '%s'." % (len(tiles), schema_tiles))
     except:
         db.conn.rollback()
-        print("Cannot create BAG tiles in schema 'bag_tiles'")
+        print("Cannot create BAG tiles in schema '%s'" % schema_tiles)
 
