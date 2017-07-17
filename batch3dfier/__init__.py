62d0ee791c32010631e7ccaf6ddc20d72f6d22d0
Balázs Dukai <balazs.dukai@gmail.com>
2017-07-17 10:31:59 +0200

    batch3dfierapp

--- batch3dfier/__init__.py
+++ batch3dfier/__init__.py
@@ -1,3 +1,3 @@
-from .batch3dfy import __version__, __copyright__, __author__, __licence__
+from batch3dfier.batch3dfierapp import __version__, __copyright__, __author__, __licence__
 
 __all__ = ['__version__', '__copyright__', '__author__', '__licence__']
