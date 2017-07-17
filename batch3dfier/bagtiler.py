44eaed360597843cf6602893f1cd607b0df2876b
Balázs Dukai <balazs.dukai@gmail.com>
2017-07-17 22:13:23 +0200

    create_centroid_table() generic

--- batch3dfier/bagtiler.py
+++ batch3dfier/bagtiler.py
@@ -76,19 +76,23 @@ def create_tile_edges(db, schema, table, id_col, geom_col):
                          schema=schema_q, table=table_q)
     db.sendQuery(sql_query)
     
-#     db.conn.commit()
-    
     db.vacuum(schema, table)
 
 
-
-def create_centroid_table(db):
+def create_centroid_table(db, schema, table_centroid, table_poly, id_col, geom_col):
     """Creates a table of building footprint centroids in a standard BAG database
 
     Parameters
     ----------
     db : db Class instance
-        
+    schema : str
+        Name of the schema that contains the 2D polygons.
+    table_centroid : str
+        Name of the new table that contains the 2D polygon centroids.
+    table_poly : str
+        Name of the table of the 2D polygons.
+    geom_col : str
+        The geometry field in table_poly.
 
     Returns
     -------
@@ -96,24 +100,37 @@ def create_centroid_table(db):
         indicating success/failure
 
     """
+    schema_q = sql.Identifier(schema)
+    table_centroid_q = sql.Identifier(table_centroid)
+    table_poly_q = sql.Identifier(table_poly)
+    geom_col_q = sql.Identifier(geom_col)
+    id_col_q = sql.Identifier(id_col)
     
-    db.sendQuery("""
-        CREATE TABLE bagactueel.pand_centroid AS
-            SELECT gid, st_centroid(geovlak)::geometry(point, 28992) AS geom
-            FROM bagactueel.pandactueelbestaand;
+    sql_query = sql.SQL("""
+        CREATE TABLE {schema}.{table_centroid} AS
+            SELECT {id_col}, st_centroid({geom_col})::geometry(point, 28992) AS geom
+            FROM {schema}.{table_poly};
         
-        SELECT populate_geometry_columns('bagactueel.pand_centroid'::regclass);
+        SELECT populate_geometry_columns({sch_tbl}::regclass);
         
         CREATE
-            INDEX pand_centroid_geom_idx ON
-            bagactueel.pand_centroid
+            INDEX {tbl_idx} ON
+            {schema}.{table_centroid}
                 USING gist(geom);
-        """)
+        """).format(schema=schema_q,
+                    table_centroid=table_centroid_q,
+                    geom_col=geom_col_q,
+                    id_col=id_col_q,
+                    table_poly=table_poly_q,
+                    sch_tbl=sql.Literal(schema + '.' + table_centroid),
+                    tbl_idx=sql.Identifier(table_centroid + '_geom_idx')
+                    )
+        
+    db.sendQuery(sql_query)
     
-    db.conn.commit()
-    
-    db.vacuum("bagactueel", "pand_centroid")
-    
+    db.vacuum(schema, table_centroid)
+
+
 def bagtiler(db):
     """Creates the BAG tiles (or Views) in the bag_tiles schema.
 
