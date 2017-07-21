471b89190b70ac3cdafa78833ff085a513b4baea
Balázs Dukai <balazs.dukai@gmail.com>
2017-07-21 17:05:28 +0200

    remove try-except from create_views because it was hiding the errors

--- db_setup.sh
+++ db_setup.sh
@@ -6,21 +6,21 @@ create database batch3dfier_testing owner batch3dfier_tester;
 create extension postgis;
 \c batch3dfier_testing batch3dfier_tester localhost
 create schema ahn3;
-create schema bagactueel;
+create schema bag;
 \q
 
 ogr2ogr -f PostgreSQL PG:"dbname=batch3dfier_testing active_schema=tile_index\
  host=localhost port=5432 user=batch3dfier_tester password=batch3d_test"\
- ./data/bag_index.geojson -nln bag_index\
+ ./example_data/bag_index.geojson -nln bag_index\
  -skip-failure -a_srs EPSG:28992 -lco FID=gid -lco GEOMETRY_NAME=geom
  
 ogr2ogr -f PostgreSQL PG:"dbname=batch3dfier_testing active_schema=tile_index\
  host=localhost port=5432 user=batch3dfier_tester password=batch3d_test"\
- ./data/ahn_index.geojson -nln ahn_index\
+ ./example_data/ahn_index.geojson -nln ahn_index\
  -skip-failure -a_srs EPSG:28992 -lco FID=gid -lco GEOMETRY_NAME=geom
  
-ogr2ogr -f PostgreSQL PG:"dbname=batch3dfier_testing active_schema=bagactueel\
+ogr2ogr -f PostgreSQL PG:"dbname=batch3dfier_testing active_schema=bag\
  host=localhost port=5432 user=batch3dfier_tester password=batch3d_test"\
- ./data/bag_pand.geojson -nln pand\
- -skip-failure -a_srs EPSG:28992 -lco FID=gid -lco GEOMETRY_NAME=geovlak
+ ./example_data/bag_pand.geojson -nln pand\
+ -skip-failure -a_srs EPSG:28992 -lco FID=gid -lco GEOMETRY_NAME=geom
 
